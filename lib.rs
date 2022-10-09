046293b3e994890771df674eb4b5df1f4b2804a1
Ryan D Johnson <ryandjohnson@gmail.com>
2022-10-09 16:33:02 -0700

    Change part1/part2 to assume a numeric result

--- lib.rs
+++ lib.rs
@@ -6,10 +6,12 @@ pub fn input() -> String {
     fs::read_to_string("input.txt").expect("Can't find input.txt")
 }
 
-pub fn part1() {
+pub fn part1() -> usize {
+    0
 }
 
-pub fn part2() {
+pub fn part2() -> usize {
+    0
 }
 
 #[cfg(test)]
@@ -18,11 +20,11 @@ mod test {
     
     #[test]
     fn run_part1() {
-        part1();
+        assert_eq!(usize::MAX, part1());
     }
 
     #[test]
     fn run_part2() {
-        part2();
+        assert_eq!(usize::MAX, part2());
     }
 }
