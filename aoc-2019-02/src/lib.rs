5d8a408c41d64f7561aca1d509d51f606d5f9707
Ryan D Johnson <ryandjohnson@gmail.com>
2022-10-09 17:32:14 -0700

    Refactor the intcode computer

--- aoc-2019-02/src/lib.rs
+++ aoc-2019-02/src/lib.rs
@@ -1,13 +1,94 @@
 // -*- compile-command: "cargo test -- --show-output" -*-
 
-#![allow(dead_code)]
-
 use std::fs;
 
-const ADD: usize = 1; // apos bpos outpos
-const MUL: usize = 2;
-const HALT: usize = 99;
-const STEP: usize = 4;
+struct Computer {
+    memory: Vec<usize>,
+    ip: usize,
+    halted: bool,
+}
+
+const ADDR_NOUN: usize = 1;
+const ADDR_VERB: usize = 2;
+
+const OP_ADD: usize = 1;
+const OP_MUL: usize = 2;
+const OP_HALT: usize = 99;
+
+enum Op {
+    Add(usize, usize, usize),
+    Mul(usize, usize, usize),
+    Halt
+}
+
+impl Computer {
+    fn new(memory: Vec<usize>) -> Self {
+        Computer { memory, ip: 0, halted: false }
+    }
+
+    fn binary_op<F>(&mut self, pa: usize, pb: usize, pc: usize, f: F)
+    where
+        F: FnOnce(usize, usize) -> usize
+    {
+        let a = self.read(pa);
+        let b = self.read(pb);
+        let c = f(a, b);
+        self.write(pc, c);
+    }
+
+    fn compute(&mut self) {
+        assert!(!self.halted);
+        assert_eq!(0, self.ip);
+
+        while !self.halted {
+            let op = self.read_next_instruction();
+            self.execute(op);
+        }
+    }
+
+    fn execute(&mut self, op: Op) {
+        match op {
+            Op::Add(pa, pb, pc) => self.binary_op(pa, pb, pc, |a, b| a + b),
+            Op::Mul(pa, pb, pc) => self.binary_op(pa, pb, pc, |a, b| a * b),
+            Op::Halt => self.halted = true,
+        }
+    }
+
+    fn read(&self, p: usize) -> usize {
+        *self.memory.get(p).unwrap()
+    }
+
+    fn read_and_advance(&mut self) -> usize {
+        let n = self.read(self.ip);
+        self.ip += 1;
+        n
+    }
+
+    fn read_next_instruction(&mut self) -> Op {
+        let opcode = self.read_and_advance();
+        match opcode {
+            OP_ADD => Op::Add(self.read_and_advance(), self.read_and_advance(), self.read_and_advance()),
+            OP_MUL => Op::Mul(self.read_and_advance(), self.read_and_advance(), self.read_and_advance()),
+            OP_HALT => Op::Halt,
+            x => panic!("Unknown opcode {x}")
+        }
+    }
+
+    fn restore_state(&mut self, noun: usize, verb: usize) {
+        self.write(ADDR_NOUN, noun);
+        self.write(ADDR_VERB, verb);
+    }
+
+    fn result(&self) -> usize {
+        assert!(self.halted);
+        self.read(0)
+    }
+
+    fn write(&mut self, p: usize, n: usize) {
+        let p = self.memory.get_mut(p).unwrap();
+        *p = n;
+    }
+}
 
 pub fn input() -> String {
     fs::read_to_string("input.txt").expect("Can't find input.txt")
@@ -17,72 +98,39 @@ fn initial_state() -> Vec<usize> {
     input().trim().split(",").map(|s| s.parse().unwrap()).collect()
 }
 
-fn result(intcodes: &Vec<usize>) -> usize {
-    *intcodes.get(0).unwrap()
+pub fn part1() -> usize {
+    let mut computer = Computer::new(initial_state());
+    computer.restore_state(12, 2);
+    computer.compute();
+    computer.result()
 }
 
-pub fn part1() {
-    let mut intcodes: Vec<usize> = initial_state();
-
-    restore_state(&mut intcodes, 12, 2);
-
-    let intcodes = compute(intcodes);
-
-    println!("result = {}", result(&intcodes));
-}
-
-pub fn part2() {
+pub fn part2() -> usize {
     let target_output = 19690720;
     let initial = initial_state();
 
     for noun in 0..=99 {
         for verb in 0..=99 {
-            let mut intcodes = initial.clone();
-            restore_state(&mut intcodes, noun, verb);
-            let intcodes = compute(intcodes);
-            if result(&intcodes) == target_output {
-                println!("noun = {noun}, verb = {verb}");
-                println!("part 2 = {}", 100 * noun + verb);
-                break;
+            let mut computer = Computer::new(initial.clone());
+            computer.restore_state(noun, verb);
+            computer.compute();
+            if computer.result() == target_output {
+                return 100 * noun + verb;
             }
         }
     }
-}
-
-fn restore_state(intcodes: &mut Vec<usize>, noun: usize, verb: usize) {
-    *intcodes.get_mut(1).unwrap() = noun;
-    *intcodes.get_mut(2).unwrap() = verb;
-}
-
-fn compute(mut intcodes: Vec<usize>) -> Vec<usize> {
-    let mut i = 0;
-    loop {
-        let op = match intcodes.get(i).unwrap() {
-            &ADD => |a, b| a + b,
-            &MUL => |a, b| a * b,
-            &HALT => break,
-            x => panic!("Unknown opcode {x}"),
-        };
-
-        let apos = *intcodes.get(i + 1).unwrap();
-        let bpos = *intcodes.get(i + 2).unwrap();
-        let outpos = *intcodes.get(i + 3).unwrap();
-
-        let a = intcodes.get(apos).unwrap();
-        let b = intcodes.get(bpos).unwrap();
-        let result = op(a, b);
-
-        let out = intcodes.get_mut(outpos).unwrap();
-        *out = result;
-
-        i += STEP;
-    }
-    intcodes
+    panic!("Not found");
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    fn compute(memory: Vec<usize>) -> Vec<usize> {
+        let mut computer = Computer::new(memory);
+        computer.compute();
+        computer.memory
+    }
 
     #[test]
     fn test_compute() {
@@ -97,11 +145,11 @@ mod test {
 
     #[test]
     fn run_part1() {
-        part1();
+        assert_eq!(6327510, part1());
     }
 
     #[test]
     fn run_part2() {
-        part2();
+        assert_eq!(4112, part2());
     }
 }
