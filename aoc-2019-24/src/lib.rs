d31b529e4b0c0951adcead19b57396c3fc814ce8
Ryan D Johnson <ryandjohnson@gmail.com>
2022-11-05 00:03:15 -0700

    Hardcode the adjacency masks for part1

--- aoc-2019-24/src/lib.rs
+++ aoc-2019-24/src/lib.rs
@@ -3,58 +3,101 @@
 use std::collections::HashSet;
 use std::fs;
 
+const RECURSIVE_STEPS: usize = 200;
+const DIM: usize = 5;
+
+const ADJACENCY_MASKS: [u32; DIM * DIM] = [
+    0b0000000000000000000100010,
+    0b0000000000000000001000101,
+    0b0000000000000000010001010,
+    0b0000000000000000100010100,
+    0b0000000000000001000001000,
+    0b0000000000000010001000001,
+    0b0000000000000100010100010,
+    0b0000000000001000101000100,
+    0b0000000000010001010001000,
+    0b0000000000100000100010000,
+    0b0000000001000100000100000,
+    0b0000000010001010001000000,
+    0b0000000100010100010000000,
+    0b0000001000101000100000000,
+    0b0000010000010001000000000,
+    0b0000100010000010000000000,
+    0b0001000101000100000000000,
+    0b0010001010001000000000000,
+    0b0100010100010000000000000,
+    0b1000001000100000000000000,
+    0b0001000001000000000000000,
+    0b0010100010000000000000000,
+    0b0101000100000000000000000,
+    0b1010001000000000000000000,
+    0b0100010000000000000000000,
+];
+
+fn compute_masks() {
+    let mut bit = 1;
+    println!("[");
+    for r in 0..DIM {
+        for c in 0..DIM {
+            // 9876543210
+            let mut mask = 0;
+            if r > 0 {
+                mask |= bit >> DIM;
+            }
+            if c > 0 {
+                mask |= bit >> 1;
+            }
+            if c + 1 < DIM {
+                mask |= bit << 1;
+            }
+            if r + 1 < DIM {
+                mask |= bit << DIM;
+            }
+
+            println!("{:025b},", mask);
+
+            bit <<= 1;
+        }
+    }
+    println!("]");
+}
+
 type BugState = u32;
 type AdventResult = BugState;
 
+struct RecursiveBugs {
+    state: [[BugState; 2 * RECURSIVE_STEPS]; 2],
+    current: usize,
+}
+
 struct Bugs {
     state: BugState,
-    dim: usize,
 }
 
 impl Bugs {
-    fn new(state: BugState, dim: usize) -> Self {
-        Bugs { state, dim }
+    fn new(state: BugState) -> Self {
+        Bugs { state }
     }
 
     fn advance(&mut self) {
         let mut new_state = 0;
         let mut bit = 1;
-        for r in 0..self.dim {
-            for c in 0..self.dim {
-                // 9876543210
-                let mut mask = 0;
-                if r > 0 {
-                    mask |= bit >> self.dim;
-                }
-                if c > 0 {
-                    mask |= bit >> 1;
-                }
-                if c + 1 < self.dim {
-                    mask |= bit << 1;
-                }
-                if r + 1 < self.dim {
-                    mask |= bit << self.dim;
-                }
 
-                let masked_state = self.state & mask;
-                let bugs_adjacent = masked_state.count_ones();
+        for i in 0..DIM * DIM {
+            let mask = ADJACENCY_MASKS[i];
 
-                let has_bug = 0 != self.state & bit;
+            let masked_state = self.state & mask;
+            let bugs_adjacent = masked_state.count_ones();
 
-            // println!("bit    = {:025b}\n\
-            //           mask   = {:025b}\n\
-            //           masked = {:025b}\n\
-            //           bugs = {}; has_bug = {}\n\
-            //           ====", bit, mask, masked_state, bugs_adjacent, has_bug);
+            let has_bug = 0 != self.state & bit;
 
-                if has_bug && 1 == bugs_adjacent {
-                    new_state |= bit;
-                } else if !has_bug && (1 == bugs_adjacent || 2 == bugs_adjacent) {
-                    new_state |= bit;
-                }
-
-                bit <<= 1;
+            if has_bug && 1 == bugs_adjacent {
+                new_state |= bit;
+            } else if !has_bug && (1 == bugs_adjacent || 2 == bugs_adjacent) {
+                new_state |= bit;
             }
+
+            bit <<= 1;
         }
 
         self.state = new_state;
@@ -64,11 +107,11 @@ impl Bugs {
         let mut s = String::new();
         let mut bit = 1;
 
-        for _ in 0..self.dim {
+        for _ in 0..DIM {
             if s.len() > 0 {
                 s += "\n";
             }
-            for _ in 0..self.dim {
+            for _ in 0..DIM {
                 s.push(if 0 == self.state & bit { '.' } else { '#' });
                 bit <<= 1;
             }
@@ -94,12 +137,10 @@ fn parse_input_part1(input: &str) -> BugState {
 }
 
 fn do_part1(input: &str) -> AdventResult {
-    const DIM: usize = 5;
-
     let mut seen = HashSet::new();
 
     let initial = parse_input_part1(input);
-    let mut bugs = Bugs::new(initial, DIM);
+    let mut bugs = Bugs::new(initial);
 
     while !seen.contains(&bugs.state) {
         seen.insert(bugs.state);
@@ -126,6 +167,12 @@ mod test {
     use super::*;
 
     #[test]
+    fn test_compute_masks() {
+        super::compute_masks();
+        //todo!()
+    }
+
+    #[test]
     fn part1_example() {
         let input = "....#\n\
                     #..#.\n\
@@ -133,42 +180,57 @@ mod test {
                     ..#..\n\
                     #....";
 
-        let mut bugs = Bugs::new(parse_input_part1(input), 5);
-        assert_eq!(bugs.to_string(), "\
+        let mut bugs = Bugs::new(parse_input_part1(input));
+        assert_eq!(
+            bugs.to_string(),
+            "\
             ....#\n\
             #..#.\n\
             #..##\n\
             ..#..\n\
-            #....");
+            #...."
+        );
         bugs.advance();
-        assert_eq!(bugs.to_string(), "\
+        assert_eq!(
+            bugs.to_string(),
+            "\
             #..#.\n\
             ####.\n\
             ###.#\n\
             ##.##\n\
-            .##..");
+            .##.."
+        );
         bugs.advance();
-        assert_eq!(bugs.to_string(), "\
+        assert_eq!(
+            bugs.to_string(),
+            "\
             #####\n\
             ....#\n\
             ....#\n\
             ...#.\n\
-            #.###");
+            #.###"
+        );
         bugs.advance();
-        assert_eq!(bugs.to_string(), "\
+        assert_eq!(
+            bugs.to_string(),
+            "\
             #....\n\
             ####.\n\
             ...##\n\
             #.##.\n\
-            .##.#");
+            .##.#"
+        );
         bugs.advance();
-        assert_eq!(bugs.to_string(), "\
+        assert_eq!(
+            bugs.to_string(),
+            "\
             ####.\n\
             ....#\n\
             ##..#\n\
             .....\n\
-            ##...");
-        
+            ##..."
+        );
+
         assert_eq!(2129920, do_part1(input));
     }
 
